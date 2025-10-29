916cd290cc59536fde915d5ea9eb61a7fdc70251
MuhammetTurkan <muhammet.trkn98@gmail.com>
2025-10-29 12:17:28 +0300

    desing the pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Favorite from "./pages/Favorite";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
